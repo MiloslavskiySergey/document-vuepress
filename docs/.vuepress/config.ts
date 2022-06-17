ac79c616ad2135c7f7ad79bcb29bc9996543fab6
MarionetkaX <sergey07061999@gmail.com>
2022-06-17 14:10:36 +0300

    ref: modification config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default defineUserConfig({
   // title: 'Documentation',
   // description: 'Just playing around',
   // base: '/document-vuepress/'
-  base: '/document-vuepress/',
+  base: '/',
 
   locales: {
     '/': {
